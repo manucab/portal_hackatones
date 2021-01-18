1c979bdf6047d7a085dd81720bea11eee783d1cc
carlos_fontan <fgjcarlos@gmail.com>
2021-01-18 23:07:15 +0100

    Successfully check parameters of a hackathon and create new

--- backend/validators/fieldsHackathon.js
+++ backend/validators/fieldsHackathon.js
@@ -1,11 +1,10 @@
 const Joi = require('joi');
 
 const fieldsHackathons = Joi.object({
-
-// Comprobar con objetos ..... *******
-
-  place:  Joi.string()
-      .validate(['online', 'presencial', 'semipresencial']),
+  place:  Joi.string().valid('online', 'presencial', 'semipresencial').required(),
+start_date: Joi.date().required(),
+end_date: Joi.date().required(),
+id_organizer: Joi.number().required(),
 })
 
 module.exports = {
