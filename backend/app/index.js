451c7a88b4585925d2771158364fa44c68ee80b0
carlos_fontan <fgjcarlos@gmail.com>
2021-01-19 18:19:47 +0100

    Home page ok

--- backend/app/index.js
+++ backend/app/index.js
@@ -8,27 +8,23 @@ const port = process.env.PORT || 4444;
 const app = express();
 
 // Routes
+const homeRouter = require('../routes/home');
 const adminRouter = require('../routes/admin/admin');
 const loginRouter = require('../routes/login');
 const createHackathonRouter = require('../routes/createHackathon');
 
-
-// const userRouter = require('../routes/user');
-
-
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Endpoints
+// Home
+app.use('/', homeRouter);
 // Admin
 app.use('/admin', adminRouter);
-// Landingpage
+// Login
 app.use('/login', loginRouter);
-
-
 
 // app.post('/validate', validateAccount);
 // app.post('/register', register);
@@ -37,22 +33,19 @@ app.use('/login', loginRouter);
 // app.get('/hackathon/:filters', filterHackathons);
 // app.get('/hackathon/:id', getHackathonInfo);
 
-// //Hackathon Page
-// app.post('/hackathon/id/enroll', isAuthenticated, enrollHackathon); // Inscribirse a un hackathon
+// Hackathon Page
+// app.post('/hackathon/id/enroll', isAuthenticated, enrollHackathon);  Inscribirse a un hackathon
 // app.post('/hackathon/id/comments', isAuthenticated, postComment);
 // app.post('/hackathon/id/comments/id', isAuthenticated, isRightUser, modifyComment);
-
 
 // Create Hackathon Page
 app.use('/createhackathon', createHackathonRouter);
 
 //// ********************************
 
-
-
 // app.use('/user', userRouter);
 
-// // Test connection db
+//  Test connection db
 // const db = require('../db/connection');
 
 // console.log(db.getConnection());
@@ -67,14 +60,13 @@ app.use('/createhackathon', createHackathonRouter);
 
 // dbtest();
 
-
 app.listen(port, err => {
-    if (err) {
-        return console.log('ERROR', err);
-    }
-    console.log(`listening on port ${port}`);
+  if (err) {
+    return console.log('ERROR', err);
+  }
+  console.log(`listening on port ${port}`);
 
-    /*
+  /*
     let messageListen = (err) ? `Error, $ {err}` : `listening on port ${port}`;
    return console.log(messageListen);
     */
