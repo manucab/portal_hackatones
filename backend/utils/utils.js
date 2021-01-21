7370812993ff664f6200b477dae185f80bcb93ba
carlos_fontan <fgjcarlos@gmail.com>
2021-01-21 15:45:58 +0100

    Add schema for confirm account

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,22 +1,30 @@
 const sendgrid = require("@sendgrid/mail");
+const {schemaValidateAccount} = require('../views/schemaValidateAccount');
 
 const sendConfirmationMail = async (email, link) => {
     sendgrid.setApiKey(process.env.EMAIL_API_KEY);
 
+    // const message = {
+    //     to: email,
+    //     from: 'furboenvena10@gmail.com',
+    //     subject: 'Validate your account',
+    //     text: `La dirección de verificación es: ${link}`,
+    //     html: `
+    //     <div>
+    //       <h1> Valida tu registro </h1>
+    //       <p> Si te has registrado en el sistema, accede al siguiente
+    //       enlace para validar tu cuenta </p>
+    //
+    //       ${link}
+    //     </div>
+    //   `,
+    // };
     const message = {
         to: email,
         from: 'furboenvena10@gmail.com',
         subject: 'Validate your account',
         text: `La dirección de verificación es: ${link}`,
-        html: `
-        <div>
-          <h1> Valida tu registro </h1>
-          <p> Si te has registrado en el sistema, accede al siguiente
-          enlace para validar tu cuenta </p>
-
-          ${link}
-        </div>
-      `,
+        html: schemaValidateAccount(email, link),
     };
 
     // Send message
