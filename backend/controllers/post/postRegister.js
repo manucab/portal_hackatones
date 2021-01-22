b17c7d4eafbc0f3c655ea9c93d80d11a8b6fa56a
Carlos Fontán <fgjcarlos@gmail.com>
2021-01-22 11:58:50 +0100

    add register new user, endpoint ('/register)

--- backend/controllers/post/postRegister.js
+++ backend/controllers/post/postRegister.js
@@ -1,33 +1,59 @@
 // Variables && instances
 require('dotenv').config();
+const { registerNewUser } = require('../../db/adminRoot/db_Insert');
+const { getUserDB } = require('../../db/adminRoot/db_Select');
+const { loginValidator } = require('../../validators/validateLogin');
+const cryptoRandomString = require('crypto-random-string');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { loginValidator } = require('../validators/validateLogin');
-const { fieldsHackathons } = require('../validators/fieldsHackathon');
-const { getUserDB } = require('../db/select');
-const { createHackathonDB } = require('../db/Insert');
+const utils = require('../../utils/utils');
 
-// Register
-const register = async(req, res) => {
+const newUser = async(req, res) => {
 
-    // // 1. Get params
-    // const {email, password} = req.body
-    //
-    // console.log('Email, password', email, password);
-    //
-    // try {
-    //
-    //   // 2. Check if the parameters are valid
-    //   const validParams = await loginValidator.validateAsync({email, password});
-    //
-    //   if (!validParams) {
-    //     console.log('Error in valid params');
-    //     res.status(401).send();
-    //     return
-    //   }
+    // 1. Get params
+    const { email, name, surname, register_date, professional_profile, rol, password } = req.body
+
+    try {
+
+        // 2. Check if the parameters are valid
+        const validParams = await loginValidator.validateAsync({ email, password });
+
+        if (!validParams) {
+            console.log('Error in valid params');
+            res.status(401).send();
+            return;
+        }
+
+        // 2.1 Check that this user not exist in Database
+        const userDB = await getUserDB(email);
+
+        if (userDB) {
+            console.log('A user with that name already exists');
+            res.status(401).send();
+            return;
+        }
+
+        // 3. Post new admin
+        // Encrypt password
+        const passwordEncrypt = await bcrypt.hash(password, 10);
+        // Generate code for url validation
+        const validationCode = cryptoRandomString({ length: parseInt(process.env.CODE_LEN), type: 'alphanumeric' });
+
+        let result = await registerNewUser(email, name, surname, register_date, professional_profile, rol, passwordEncrypt, validationCode);
+
+        result = (result === undefined) ? '[]' : result;
+
+        // 4. Send email to confirm count
+        await utils.sendConfirmationMail(email, `http://${process.env.PUBLIC_DOMAIN}/user/validate/${validationCode}`)
+
+        console.log('Register new user sucessfull');
+        res.send('Register new user sucessfull');
+    } catch (e) {
+        console.log('Error post new user db', 'e=>', e);
+        res.status(500).send();
+    }
 
 }
 
 module.exports = {
-    register
+    newUser
 }
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 // Controller
-const {newUser} = require('../controllers/admin/newUser');
+const { newUser } = require('../controllers/post/postRegister');
 
 // Middelware
-const  {isUser} =  require('../middelwares/authorization/auth');
+const { isUser } = require('../middelwares/authorization/auth');
 
 
 // **** POST *****
 // Register new user in db
-router.post('/',isUser,newUser);
+router.post('/', isUser, newUser);
 
 module.exports = router;
