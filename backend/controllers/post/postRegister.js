439ed042ee01b53fcbd5a17186cb63fde7048d30
Carlos Fontán <fgjcarlos@gmail.com>
2021-01-23 22:01:11 +0100

    Add register ok

--- backend/controllers/post/postRegister.js
+++ backend/controllers/post/postRegister.js
@@ -4,19 +4,29 @@ const { registerNewUser } = require('../../db/adminRoot/db_Insert');
 const cryptoRandomString = require('crypto-random-string');
 const bcrypt = require('bcrypt');
 const utils = require('../../utils/utils');
+const { performQuery } = require('../../db/performQuery');
+
+let query = '';
+let params = [];
 
 const newUser = async(req, res) => {
 
     // 1. Get params
-    const { email, name, surname, register_date, professional_profile, rol, password } = req.body
+    const { email, name, surname, register_date, professional_profile, rol, password } = req.body;
 
     try {
 
         // 3. Post new admin
         // Encrypt password
-        const passwordEncrypt = await bcrypt.hash(password, 10);
+        const passwordEncrypt = await bcrypt.hash(password, parseInt(process.env.PASSWORD_LEN));
+
         // Generate code for url validation
         const validationCode = cryptoRandomString({ length: parseInt(process.env.CODE_LEN), type: 'alphanumeric' });
+
+        //Start transaction mysql
+        query = 'start transaction';
+        await performQuery(query, params);
+        console.log('Init transaction query');
 
         // Insert into db new user
         // TODO -- Check format date 
@@ -25,15 +35,20 @@ const newUser = async(req, res) => {
         let link = `http://${process.env.PUBLIC_DOMAIN}/user/validate/${insertId}/${validationCode}`;
 
         // 4. Send email to confirm count
-        await utils.sendConfirmationMail(email, link)
+        await utils.sendConfirmationMail(email, link);
 
         console.log('Register new user sucessfull');
         res.send('Register new user sucessfull');
     } catch (e) {
 
-        let msgError = e.message || 'Error in login';
+        // Something wrong --> Rollback
+        query = 'rollback';
+        await performQuery(query, params);
+        console.log('Rollback query');
 
-        console.log('Error post new user db', 'e=>', msgError);
+        let msgError = e.message || 'Error in postRegister';
+
+        console.log('Error postRegister', 'e=>', msgError, e);
         res.status(500).send(msgError);
     }
 
--- backend/db/adminRoot/db_Insert.js
+++ backend/db/adminRoot/db_Insert.js
@@ -1,38 +1,38 @@
 require('dotenv').config();
-const {performQuery} = require('../../db/performQuery');
+const { performQuery } = require('../../db/performQuery');
 
-const registerNewUser = async (email, name, surname, register_date, professional_profile, rol, passwordEncrypt, validationCode) => {
+const registerNewUser = async(email, name, surname, register_date, professional_profile, rol, passwordEncrypt, validationCode) => {
 
-  const query = 'insert into competitor (email,user_name,surname,register_date,professional_profile,rol, user_password, code) values( ? , ? , ? , ? , ?, ?, ?, ?)';
+    const query = 'insert into competitor (email,user_name,surname,register_date,professional_profile,rol, user_password, code) values( ? , ? , ? , ? , ?, ?, ?, ?)';
 
-  const params = [
-    email,
-    name,
-    surname,
-    register_date,
-    professional_profile,
-    rol,
-    passwordEncrypt,
-    validationCode
-  ];
+    const params = [
+        email,
+        name,
+        surname,
+        register_date,
+        professional_profile,
+        rol,
+        passwordEncrypt,
+        validationCode
+    ];
 
-  const result = await performQuery(query, params);
+    const result = await performQuery(query, params);
 
-  return result;
+    return result;
 }
 
-const registerNewAdmin = async (name, email, password, key_admin) => {
+const registerNewAdmin = async(name, email, password, key_admin) => {
 
-  const query = 'insert into admin (key_admin, name, email, password) values (?,?,?,?)';
+    const query = 'insert into admin (key_admin, name, email, password) values (?,?,?,?)';
 
-  const params = [key_admin, name, email, password];
+    const params = [key_admin, name, email, password];
 
-  const result = await performQuery(query, params);
+    const result = await performQuery(query, params);
 
-  return result;
+    return result;
 }
 
 module.exports = {
-  registerNewUser,
-  registerNewAdmin
+    registerNewUser,
+    registerNewAdmin
 }
