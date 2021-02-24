9066c89a3cbcbdb1186f33cb716109addd048ff5
Manu <manuelcabezas.a@gmail.com>
2021-02-24 17:24:41 +0100

    Merge

--- front/portal_hackatones/src/Components/Footer/Footer.js
+++ front/portal_hackatones/src/Components/Footer/Footer.js
@@ -1,55 +1,81 @@
-import {Link} from 'react-router-dom';
-import './Footer.css'
-import logoHome from '../../Media/Images/General/logo.jpg';
-import logoBlog from '../../Media/Images/General/blogLogo.png';
-import logoHackathon from '../../Media/Images/General/codeLogo.svg';
-import logoAbout from '../../Media/Images/General/about.svg';
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import logoHome from "../../Media/Images/General/logo.jpg";
+import logoBlog from "../../Media/Images/General/blogLogo.png";
+import logoHackathon from "../../Media/Images/General/codeLogo.svg";
+import logoAbout from "../../Media/Images/General/about.svg";
+import MediaQuery from "react-responsive";
 
 function Footer() {
+  return (
+    <footer className="footer">
+      <MediaQuery maxWidth={767}>
+        <div>
+          <Link to="/" className="logoFooter">
+            <img id="homeButton" src={logoHome} alt="home-logo" />
+            <span>Inicio</span>
+          </Link>
+        </div>
 
-    return (
-        <footer className="footer">
-            <div>
-                <Link to="/" className="logoFooter">
+        <div>
+          <Link to="/hackathon/search" className="logoFooter">
+            <img
+              id="hackathonsButton"
+              src={logoHackathon}
+              alt="hackathon-logo"
+            />
+            <span>Hackatones</span>
+          </Link>
+        </div>
 
-                    <img id="homeButton"
-                        src={logoHome}
-                        alt="home-logo"/>
-                    <span>Inicio</span>
-                </Link>
+        <div>
+          <Link to="/blog" className="logoFooter">
+            <img id="hackathonsButton" src={logoBlog} alt="blog-logo" />
+            <span>Blog</span>
+          </Link>
+        </div>
+
+        <div>
+          <Link to="/about" className="logoFooter">
+            <img id="hackathonsButton" src={logoAbout} alt="about-logo" />
+            <span>Nosotros</span>
+          </Link>
+        </div>
+      </MediaQuery>
+      <MediaQuery minWidth={768}>
+        <div className="footer-container-wide">
+          <div>
+            <Link to="/" id="logoFooter-wide">
+              <img id="homeButton" src={logoHome} alt="home-logo" />
+            </Link>
+          </div>
+          <div className="footer-links-wide">
+            <div className="footer-link-wide">
+              <Link to="/hackathon/search" className="logoFooter">
+                <span>Hackatones</span>
+              </Link>
             </div>
-
-            <div>
-                <Link to="/hackathon/search" className="logoFooter">
-                    <img id="hackathonsButton"
-                        src={logoHackathon}
-                        alt="hackathon-logo"/>
-                    <span>Hackatones</span>
-
-                </Link>
+            <div className="footer-link-wide">
+              <Link to="/blog" className="logoFooter">
+                <span>Blog</span>
+              </Link>
             </div>
-
-            <div>
-                <Link to="/blog" className="logoFooter">
-                    <img id="hackathonsButton"
-                        src={logoBlog}
-                        alt="blog-logo"/>
-                    <span>Blog</span>
-                </Link>
+            <div className="footer-link-wide">
+              <Link to="/about" className="logoFooter">
+                <span>About</span>
+              </Link>
             </div>
-
-            <div>
-                <Link to="/about" className="logoFooter">
-                    <img id="hackathonsButton"
-                        src={logoAbout}
-                        alt="about-logo"/>
-                    <span>Nosotros</span>
-                </Link>
+            <div className="copyright">
+              <span>
+                © December 2020 Unity Hackathons S.L.U. Todos los derechos
+                reservados.
+              </span>
             </div>
-
-
-        </footer>
-    );
+          </div>
+        </div>
+      </MediaQuery>
+    </footer>
+  );
 }
 
 export default Footer;
--- front/portal_hackatones/src/Components/Header/Header.js
+++ front/portal_hackatones/src/Components/Header/Header.js
@@ -1,28 +1,86 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Link} from 'react-router-dom';
-import Avatar from '../Avatar/Avatar';
-import './Header.css'
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Avatar from "../Avatar/Avatar";
+import "./Header.css";
+import MediaQuery from "react-responsive";
+import logoHome from '../../Media/Images/General/logo.jpg';
+import logoBlog from '../../Media/Images/General/blogLogo.png';
+import logoHackathon from '../../Media/Images/General/codeLogo.svg';
+import logoAbout from '../../Media/Images/General/about.svg';
 
 function Header() {
-  const login = useSelector(s => s.login)
-  const id = login ? login.user.id:null
-  const profileUrl = `/user/${id}`
-  
+  const login = useSelector((s) => s.login);
+  const id = login ? login.user.id : null;
+  const profileUrl = `/user/${id}`;
 
   return (
     <header>
-        <Link to="/"><div className="logo"></div></Link> 
-        <h1>Hackathons Place</h1> 
+      <MediaQuery maxWidth={767}>
+        <Link to="/">
+          <div className="logo"></div>
+        </Link>
+        <h1>Hackathons Place</h1>
 
+        {!login && (
+          <Link className="loginButton" to="/login">
+            <span>Sign In</span>{" "}
+          </Link>
+        )}
+        {login && (
+          <div className="loginSection">
+            <Link to={profileUrl}>
+              {" "}
+              <Avatar />{" "}
+            </Link>
+          </div>
+        )}
+      </MediaQuery>
+      <MediaQuery minWidth={768}>
+        <Link to="/">
+          <div className="logo"></div>
+        </Link>
+        <div>
+                <Link to="/hackathon/search" className="logoFooter">
+                    <img id="hackathonsButton"
+                        src={logoHackathon}
+                        alt="hackathon-logo"/>
+                    <span>Hackatones</span>
 
-        {!login &&
-            <Link className="loginButton" to="/login"><span>Sign In</span>  </Link>
-        }
-        {login &&
-            <div className="loginSection">
-                <Link to={profileUrl} > <Avatar/> </Link>
+                </Link>
             </div>
-        }
+
+            <div>
+                <Link to="/blog" className="logoFooter">
+                    <img id="hackathonsButton"
+                        src={logoBlog}
+                        alt="blog-logo"/>
+                    <span>Blog</span>
+                </Link>
+            </div>
+
+            <div>
+                <Link to="/about" className="logoFooter">
+                    <img id="hackathonsButton"
+                        src={logoAbout}
+                        alt="about-logo"/>
+                    <span>Nosotros</span>
+                </Link>
+            </div>
+        
+        {!login && (
+          <Link className="loginButton" to="/login">
+            <span>Sign In</span>{" "}
+          </Link>
+        )}
+        {login && (
+          <div className="loginSection">
+            <Link to={profileUrl}>
+              {" "}
+              <Avatar />{" "}
+            </Link>
+          </div>
+        )}
+      </MediaQuery>
     </header>
   );
 }
