22635361d617ea1ec7d3154103d3fa18e70f466c
Carlos Fontán <fgjcarlos@gmail.com>
2021-02-24 17:14:56 +0100

    fetch enroll

---
+++ front/portal_hackatones/src/Hooks/useFetchPostEnrrol.js
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+
+function useFetchPostEnrrol(params, registerOk, url, login) {
+
+    const [data, setData] = useState();
+
+    if(!login){
+        login = null;
+    }
+
+
+
+    useEffect(() => {
+
+        const headers = {
+            'Content-Type': 'application/json',
+      Authorization: login.token,
+
+        }
+
+        registerOk === 1 && fetch(url, {
+            headers,
+            body: JSON.stringify(params),
+            method: 'POST'
+        }).then(res => setData(res))
+            
+
+    }, [registerOk])
+    return data;
+
+}
+
+
+export default useFetchPostEnrrol;
